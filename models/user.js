2b8b5d169079374787d66400e7db8c06530f77ea
CarrieShort <carrie.r.phillips@gmail.com>
2017-11-29 11:19:03 -0800

    add asynchronous hashing using mongoose pre save method

--- models/user.js
+++ models/user.js
@@ -5,11 +5,30 @@ var userSchema = new mongoose.Schema({
   userName: {
     type: String,
     required: true,
+    unique: true
     validate: function(email) {
       return /^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)
     }
   },
   password: {
-    type: String
+    type: String,
+    required: true
   }
-})
+});
+
+userSchema.pre('save', {
+  var user = this;
+  
+  if (!user.isModified('password')) return next();
+  
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      user.password = hash;
+      next();
+    });
+    
+  });
+  
+});
