b77212621d3fa6117fe9e5335a680cbd12e4e225
CarrieShort <carrie.r.phillips@gmail.com>
2017-11-29 10:42:42 -0800

    add html5 email validation regex from w3

--- models/user.js
+++ models/user.js
@@ -3,7 +3,11 @@ const bcrypt = require('bcrypt-nodejs');
 
 var userSchema = new mongoose.Schema({
   userName: {
-    type: String
+    type: String,
+    required: true,
+    validate: function(email) {
+      return /^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)
+    }
   },
   password: {
     type: String
