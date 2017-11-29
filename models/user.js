57d33d1f3b26c59a334a58436a6a26f897402d40
CarrieShort <carrie.r.phillips@gmail.com>
2017-11-29 13:52:53 -0800

    add unit test for password hash

--- models/user.js
+++ models/user.js
@@ -16,18 +16,19 @@ var userSchema = new mongoose.Schema({
   }
 });
 
-userSchema.pre('save', (next, done) => {
+userSchema.pre('save', function(next, done) {
   var user = this;
   
   if (!user.isModified('password')) return next();
   
-  bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.genSalt(10, function(err, salt) {
     if (err) return next(err);
     
-    bcrypt.hash(user.password, salt, null, (err, hash) => {
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
       if (err) return next(err);
       user.password = hash;
       next();
+      done();
     });
     
   });
--- test/auth_test.js
+++ test/auth_test.js
@@ -3,7 +3,10 @@ const chaiHttp = require('chai-http');
 const expect = chai.expect;
 chai.use(chaiHttp);
 const port = process.env.PORT = 5000;
+const mongoose = require('mongoose');
 const User = require(__dirname + '/../models/user');
+// process.env.MONGODB_URI = 'mongodb://localhost/testdb';
+// const server = require(__dirname + '/../server');
 describe('the User schema', () => {
   it('should require name and password', (done) => {
     var newUser = new User();
@@ -17,6 +20,14 @@ describe('the User schema', () => {
     var newUser = new User({email: 'i am a test', password: 'testpass'});
     newUser.validate((err) => {
       expect(newUser.errors.email).to.exist;
+      done();
+    });
+  });
+  it('should hash the password before saving', (done) => {
+    var newUser = new User({email: 'blue@test.com', password: 'testpass'});
+    newUser.save((err) => {
+      expect(newUser.email).to.equal('blue@test.com');
+      expect(newUser.password).to.not.equal('testpass');
       done();
     });
   });
