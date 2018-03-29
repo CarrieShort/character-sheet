50037f1c96fad0106532e74217e15ff932593376
CarrieShort <carrie.r.phillips@gmail.com>
2018-03-29 16:34:26 -0700

    create onInputChange function to update state when input field is changed

--- src/components/abilities.js
+++ src/components/abilities.js
@@ -11,6 +11,12 @@ class Abilities extends Component {
       charisma: ''
     }
   }
+  onInputChange = e => {
+    var inputName = e.target.name
+    this.setState({
+      abilities: {...this.state.abilities, [inputName]: e.target.value}
+    })
+  }
   handleSubmit = (e) => {
     e.preventDefault();
     console.log(e)
@@ -21,12 +27,12 @@ class Abilities extends Component {
       <section>
       <h2>Abilities</h2>
         <form className="form abilities" onSubmit={this.handleSubmit}>
-          <input type="text" name="strenght" />
-          <input type="text" name="dexterity" />
-          <input type="text" name="constitution" />
-          <input type="text" name="intelligence" />
-          <input type="text" name="wisdom" />
-          <input type="text" name="charisma" />
+          <input type="text" name="strength" onChange={this.onInputChange} value={this.state.abilities.strength} />
+          <input type="text" name="dexterity" onChange={this.onInputChange} value={this.state.abilities.dexterity}/>
+          <input type="text" name="constitution" onChange={this.onInputChange} value={this.state.abilities.constitution}/>
+          <input type="text" name="intelligence" onChange={this.onInputChange} value={this.state.abilities.intelligence}/>
+          <input type="text" name="wisdom" onChange={this.onInputChange} value={this.state.abilities.wisdom}/>
+          <input type="text" name="charisma" onChange={this.onInputChange} value={this.state.abilities.charisma}/>
           <button type="submit">Save</button>
         </form>
       </section>
